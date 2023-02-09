ad7743f9637ba6fe37102741e22b12630a330f50
Gisselle Pombar <gisselle.pombar@gmail.com>
2023-02-09 09:58:54 -0500

    framed out app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Welcome from './scenes/Welcome';
+import Content from './scenes/Content';
+import Login from './scenes/Login';
+import Signup from './scenes/Signup';
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState();
   return (
+    <BrowserRouter>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Routes>
+          {/* {user && <Route path='/secret' element={<Content />}} */}
+          <Route path='/secret' element={(user) ? <Content /> : <Welcome />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route exact path='/' element={<Welcome />} />
+        </Routes>
       </header>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/scenes/Signup.jsx
@@ -0,0 +1,5 @@
+export default function Welcome() {
+    return(
+        <h1></h1>
+    )
+}
